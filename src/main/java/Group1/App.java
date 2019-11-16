3a453a0c10fbdf7f5168944b62c56a99b3ffbbbd
Cloud User <centos@ip-172-31-27-239.us-east-2.compute.internal>
2019-11-16 14:52:03 +0000

    AppChanges

--- src/main/java/Group1/App.java
+++ src/main/java/Group1/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Ayyappan!" );
+        System.out.println( "Hello Ayyappan!,welcome to Devopps" );
     }
 }
